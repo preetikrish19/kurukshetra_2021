f00c7194128ef873b51e023cc288b2c28f3822d6
preetikrish19 <62648562+preetikrish19@users.noreply.github.com>
2021-04-03 19:04:27 +0530

    hello

--- management.php
+++ management.php
@@ -3,8 +3,25 @@
 <head>
   <title>Our Management</title>
 	<link rel="stylesheet" type="text/css" href="manage.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
+  <nav class="navbar navbar-inverse bg-dark navbar-dark">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">Digital India</a>
+        </div>
+        <ul style="display: inline" class="nav navbar-nav navbar-right">
+            <a href="index.php"><li style="display: inline; margin-right:3vh;">Home</li></a>
+            <a href="vision.php"><li style="display: inline; margin-right:3vh;">Our Vision</li></a>
+            <a href="management.php"><li style="display: inline; margin-right:3vh;">Management</li></a>
+        </ul>
+      </div>
+    </nav>
+    <br>
 	<h2 style="color: white">Our Management</h2>
 	<div class="container">
 		<img src="img/img3.webp">
@@ -14,5 +31,6 @@
 		<img src="img/img8.jpeg">
     <img src="img/img9.webp">
 	</div>
+  <?php include("footer.php"); ?>
 </body>
 </html>
