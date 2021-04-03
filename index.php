17d4e33d4b87e5913c82bab408b28fcc03c62c8e
preetikrish19 <62648562+preetikrish19@users.noreply.github.com>
2021-04-03 17:52:16 +0530

    index

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Digital India</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">Digital India</a>
+      </div>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"></a>Home</li>
+        <li><a href="#"></a>Our Vision</li>
+        <li><a href="#"></a>Manage</li>
+      </ul>
+    </div>
+  </nav>
+  </body>
+</html>
