6721bfb9f0e5400c11555e4024adc4f05c14898e
bmspr1502 <57596428+bmspr1502@users.noreply.github.com>
2021-04-03 18:25:44 +0530

    vision update

--- vision.php
+++ vision.php
@@ -12,7 +12,19 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 
-<body>
+<body style="background: #FF9671">
+<nav class="navbar navbar-inverse bg-dark navbar-dark">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">Digital India</a>
+      </div>
+      <ul style="display: inline" class="nav navbar-nav navbar-right">
+        <a href="#"><li style="display: inline; margin-right:3vh;">Home</li></a>
+        <a href="#"><li style="display: inline; margin-right:3vh;">Our Vision</li></a>
+        <a href="#"><li style="display: inline; margin-right:3vh;">Management</li></a>
+      </ul>
+    </div>
+  </nav>
     <div class='container'>
     
     <div id="demo" class="carousel slide" data-ride="carousel">
@@ -46,7 +58,54 @@
         </a>
 
         </div>
-    </div>
+
+        <div >
+       
+
+            <div class='row'>
+                <div class="col-md-4 mt-3">
+                    <div class="card  p-4 " style="background:#4B4453">
+                        <h2>Digital Infrastructure as a Core Utility to Every Citizen</h2>
+
+                        <ul class="list-group">
+                            <li class="list-group-item">Availability of high speed internet as a core utility for delivery of services to citizens</li>
+                            <li class="list-group-item">Cradle to grave digital identity that is unique, lifelong, online and authenticable to every <citizen></citizen></li>
+                            <li class="list-group-item">Mobile phone & bank account enabling citizen participation in digital & financial space</li>
+                            <li class="list-group-item">Easy access to a Common Service Centre</li>
+                            <li class="list-group-item">Safe and secure cyber-space</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4 mt-3">
+                    <div class="card p-4 " style="background:#4B4453">
+                        <h2>Governance & Services on Demand</h2>
+                        <ul class="list-group">
+                            <li class="list-group-item">Seamlessly integrated services across departments or jurisdictions</li>
+                            <li class="list-group-item">Availability of services in real time from online & mobile platforms</li>
+                            <li class="list-group-item">All citizen entitlements to be portable and available on the cloud</li>
+                            <li class="list-group-item">Digitally transformed services for improving ease of doing business</li>
+                            <li class="list-group-item">Making financial transactions electronic & cashless</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4 mt-3">
+                    <div class="card p-4 " style="background:#4B4453" >
+                        <h2>Digital Empowerment of Citizens
+                        </h2>
+                        <ul class="list-group">
+                            <li class="list-group-item">Universal digital literacy</li>
+                            <li class="list-group-item">Universally accessible digital resources</li>
+                            <li class="list-group-item">Availability of digital resources / services in Indian languages</li>
+                            <li class="list-group-item">Collaborative digital platforms for participative governance</li>
+                            <li class="list-group-item">Citizens not required to physically submit Govt. documents / certificates</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+</div>
     <?php include('footer.php') ?>
 </body>
 </html>
