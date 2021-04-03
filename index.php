949e2cae1afd50d9279be1c67fb3cf5134d5a3c1
preetikrish19 <62648562+preetikrish19@users.noreply.github.com>
2021-04-03 18:01:54 +0530

    changes

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <title>Digital India</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style media="screen">
+
+    </style>
   </head>
   <body>
   <nav class="navbar navbar-inverse">
@@ -11,12 +14,13 @@
       <div class="navbar-header">
         <a class="navbar-brand" href="#">Digital India</a>
       </div>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"></a>Home</li>
-        <li><a href="#"></a>Our Vision</li>
-        <li><a href="#"></a>Manage</li>
+      <ul style="display: inline" class="nav navbar-nav navbar-right">
+        <a href="#"><li style="display: inline; margin-right:3vh;">Home</li></a>
+        <a href="#"><li style="display: inline; margin-right:3vh;">Our Vision</li></a>
+        <a href="#"><li style="display: inline; margin-right:3vh;">Management</li></a>
       </ul>
     </div>
   </nav>
+  <?php include('footer.php') ?>
   </body>
 </html>
